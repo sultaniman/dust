bff0b6889c36dbec2de81e25ff5b0e04ef56d471
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-16 22:25:12 +0200

    Add typings to request module

--- lib/dust/request.ex
+++ lib/dust/request.ex
@@ -1,3 +1,14 @@
 defmodule Dust.Request do
   @moduledoc false
+  alias Dust.Result
+
+  @type url() :: String.t()
+  @type options() :: Keyword.t()
+
+  @spec fetch(url(), options()) :: Result.t()
+  def fetch(_url, _options) do
+    %Result{
+      content: "blabla"
+    }
+  end
 end
