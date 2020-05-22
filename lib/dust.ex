f01689753cc9fafe7e80f8b145795cbe2b27ba81
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-22 23:18:47 +0200

    Define Dust.get/2 public API

--- lib/dust.ex
+++ lib/dust.ex
@@ -2,4 +2,8 @@ defmodule Dust do
   @moduledoc """
   Documentation for `Dust`.
   """
+  use Dust.Types
+
+  @spec get(url(), options()) :: {Client.t(), Result.t()}
+  def get(url, options), do: Dust.Request.fetch(url, options)
 end
