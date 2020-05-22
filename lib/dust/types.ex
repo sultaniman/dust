1c3c000496aca34be7b2d7a7d15438decf445285
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-22 15:21:46 +0200

    Add common types

---
+++ lib/dust/types.ex
@@ -0,0 +1,21 @@
+defmodule Dust.Types do
+  @moduledoc """
+  Contains primitive type aliases and typespecs
+  """
+  defmacro __using__(_) do
+    quote do
+      # Request related types
+      @type url() :: String.t()
+      @type content() :: String.t()
+      @type links() :: list(String.t())
+      @type resource() :: map()
+
+      # For DOM elements
+      @type xpath() :: String.to()
+      @type pairs() :: list({xpath(), value()})
+
+      # General purpose types
+      @type options() :: Keyword.t() | any()
+    end
+  end
+end
