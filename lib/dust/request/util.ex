bf30deb8216629a6ad1928e8b778145903afc282
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-22 23:24:29 +0200

    Add uri normalizer

--- lib/dust/request/util.ex
+++ lib/dust/request/util.ex
@@ -12,4 +12,15 @@ defmodule Dust.Request.Util do
   def same_domain?(domain, uri) do
     true
   end
+
+  @spec normalize_url(domain(), uri()) :: String.t()
+  def normalize_url(domain, url) do
+    if String.starts_with?(url, "http") do
+      url
+    else
+      trimmed = String.trim(domain, "/")
+      path = String.trim(url, "/")
+      "#{trimmed}/#{path}"
+    end
+  end
 end
