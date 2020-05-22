7eea010192766ac8ae2e43eae21439e877fbd3dd
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-22 15:25:06 +0200

    Adjust types

--- lib/dust/result.ex
+++ lib/dust/result.ex
@@ -9,11 +9,11 @@ defmodule Dust.Result do
     field :content, String.t()
     field :status, non_neg_integer()
     field :duration, non_neg_integer()
-    field :headers, [{atom, binary}] | [{binary, binary}] | %{binary => binary} | any
-    field :images, list()
-    field :css, list()
-    field :js, list()
-    field :svg, list()
-    field :json, list()
+    field :headers, map()
+    field :images, list(String.t())
+    field :css, list(String.t())
+    field :js, list(String.t())
+    field :svg, list(String.t())
+    field :json, list(String.t())
   end
 end
