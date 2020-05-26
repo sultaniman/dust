57f52ea74f268acbb922b9857a97326ef9ec3c1a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-26 17:34:49 +0200

    Update alias paths

--- lib/dust.ex
+++ lib/dust.ex
@@ -3,7 +3,11 @@ defmodule Dust do
   Documentation for `Dust`.
   """
   use Dust.Types
+  alias Dust.Requests
+  alias Dust.Requests.{Client, Result}
 
-  @spec get(url(), options()) :: {Client.t(), Result.t()}
-  def get(url, options), do: Dust.Request.fetch(url, options)
+  @spec get(url(), headers(), options()) :: {Client.t(), Result.t()}
+  def get(url, headers, options), do: Requests.get(url, headers, options)
+
+  # Dust.get(url, headers, loaders: [:css, :js, :json, :image]) => {:ok, embedded_html, result} | {:error, nil, result}
 end
