67a2f3c8f8df6a1790d4d029d38de7dff64a3296
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-31 13:12:17 +0200

    Add notes about encoding and embedding images

--- lib/dust/loaders/image.ex
+++ lib/dust/loaders/image.ex
@@ -1,3 +1,7 @@
 defmodule Dust.Loaders.Image do
-  @moduledoc false
+  @moduledoc """
+  Embed with mime `data:image/<MIME>;base64,BASE64` for binary formats
+  for svg `data:image/svg+xml,URI_ENCODED_SVG`
+  Ignore already embedded `data:image`
+  """
 end
