a1797e9fb7bcf5b8c06610c48354a0aab087e497
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-06-05 15:57:14 +0200

    Reject all data urls for images

--- lib/dust/parsers/image.ex
+++ lib/dust/parsers/image.ex
@@ -4,6 +4,7 @@ defmodule Dust.Parsers.Image do
   scripts with absolute urls.
   """
   alias Dust.Dom
+  alias Dust.Parsers
 
   @spec parse(Floki.html_tree() | Floki.html_tag()) :: list(String.t())
   def parse(document) do
@@ -11,7 +12,8 @@ defmodule Dust.Parsers.Image do
     pictures = Dom.attr(document, "picture > source", "srcset")
 
     imgs ++ pictures
-    |> Enum.reject(&String.starts_with?(&1, "data:image"))
+    |> Enum.reject(&Parsers.URI.is_data_url?/1)
+    |> Enum.reject(&Parsers.URI.is_font?/1)
     |> MapSet.new()
     |> MapSet.to_list()
   end
