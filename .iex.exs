c2995a305fb86fca41239a60c0067665eeb7ff4e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-06-08 16:07:04 +0200

    Update aliases

--- .iex.exs
+++ .iex.exs
@@ -1,6 +1,22 @@
-alias Dust.Requests
-alias Dust.Requests.{ClientState, Result, Proxy, Util}
-alias Dust.Transformer.CSS
+alias Dust.{
+  Dom,
+  Loaders,
+  Parsers,
+  Requests,
+}
+
+alias Dust.Loaders.{
+  CSS,
+  Image,
+  JS
+}
+
+alias Dust.Requests.{
+  ClientState,
+  Result,
+  Proxy,
+  Util
+}
 
 defmodule Vars do
   def url, do: "https://hex.pm/docs/publish"
