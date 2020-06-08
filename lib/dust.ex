5c8cd3c20a7738e83d09472206503659d21375a9
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-06-09 00:38:16 +0200

    Prepare to parse and fetch CSS assets

--- lib/dust.ex
+++ lib/dust.ex
@@ -12,6 +12,14 @@ defmodule Dust do
     # Loaders.process(result, [], client: client_state)
   end
 
+  def process() do
+    proxy_uri = System.get_env("SOCKS_PROXY")
+    {:ok, result, state} = Dust.get("https://turannews.info", proxy: proxy_uri)
+    assets = Parsers.parse(result.content)
+    resources = Fetcher.fetch(assets, result.base_url, headers: state.headers, proxy: state.proxy, options: state.proxy)
+    # For each resources[:css] -> extract urls -> Fetcher.fetch() -> Append to resources[:css]
+  end
+
   def persist(path, contents) do
     with {:ok, file} <- File.open(path, [:write]) do
       IO.binwrite(file, contents)
