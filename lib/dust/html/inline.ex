b0a5d8990255f9d2acd7625572b5640e6d4b6d45
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-06-15 17:55:42 +0200

    Refactor Html.Inline

--- lib/dust/html/inline.ex
+++ lib/dust/html/inline.ex
@@ -6,26 +6,23 @@ defmodule Dust.HTML.Inline do
   @type resources() :: list(Resource.t())
 
   @spec inline(content_list(), resources()) :: content_list()
-  def inline(content, resources) do
-    content
-    |> Enum.map(fn line ->
-      resource =
-        resources
-        |> Enum.filter(&String.contains?(line, &1.relative_url))
-        |> List.first()
-
-      case resource do
-        [] -> line
-        nil -> line
-        _ ->
-          IO.inspect(resource)
-          %{result: {:ok, result, _}} = resource
-          String.replace(
-            line,
-            resource.relative_url,
-            Image.encode(resource.relative_url, result.content)
-          )
-      end
+  def inline(lines, resources) do
+    Enum.map(lines, fn line ->
+      resources
+      |> Enum.filter(&String.contains?(line, &1.relative_url))
+      |> List.first()
+      |> replace_image(line)
     end)
+  end
+
+  defp replace_image(nil, line), do: line
+  defp replace_image([], line), do: line
+  defp replace_image(%{result: {:error, _result, _}}, line), do: line
+  defp replace_image(%{result: {:ok, result, _}} = resource, line) do
+    String.replace(
+      line,
+      resource.relative_url,
+      Image.encode(resource.relative_url, result.content)
+    )
   end
 end
