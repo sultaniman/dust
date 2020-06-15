0f950fe970c4a1173cbfbaad309f234acc820d12
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-06-15 23:05:59 +0200

    Prepare mix for release

--- mix.exs
+++ mix.exs
@@ -2,13 +2,39 @@ defmodule Dust.MixProject do
   use Mix.Project
 
   @vsn "0.0.1"
+  @package [
+    name: "dust",
+    files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+    licenses: ["Apache 2.0"],
+    links: %{"GitHub" => "https://github.com/imanhodjaev/dust"}
+  ]
+
+  @description "Download web pages as a single HTML"
+
   def project do
     [
       app: :dust,
       version: @vsn,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      package: @package,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      # Docs
+      name: "Dust",
+      source_url: "https://github.com/imanhodjaev/dust",
+      homepage_url: "https://github.com/imanhodjaev/dust",
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -26,7 +52,11 @@ defmodule Dust.MixProject do
       {:retry, "~> 0.14.0"},
       {:floki, "~> 0.26.0"},
       {:typed_struct, "~> 0.1.4"},
-      {:ex_image_info, "~> 0.2.4"}
+      {:ex_image_info, "~> 0.2.4"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.12", only: :test},
+      {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
